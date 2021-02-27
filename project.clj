41366eb41d0a3cf2d055a58b20ef3a96a4149fab
Ricardo Moran <richi.moran@gmail.com>
2021-02-27 13:19:04 -0300

    Added :dev and :test profiles with a couple of settings I like.
    - proto-repl dependency
    - user namespace with refresh-all
    - test-refresh
    - sound notifications for test results
    - warnings for reflection and unchecked math

--- project.clj
+++ project.clj
@@ -7,4 +7,16 @@
   :main ^:skip-aot clojure-demo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :dev {:dependencies [[proto-repl "0.3.1"]
+                                  [org.clojure/tools.cli "1.0.194"]
+                                  [org.clojure/tools.namespace "0.3.1"]
+                                  [org.clojure/core.async "0.6.532"]
+                                  [org.clojars.beppu/clj-audio "0.3.0"]]
+                   :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]
+                   :global-vars {*unchecked-math* :warn-on-boxed
+                                 *warn-on-reflection* true}
+                   :source-paths ["env/dev/clj"]
+                   :repl-options {:init-ns user}}
+             :test {:resource-paths ["env/test/sounds"]
+                    :source-paths ["env/test/clj"]}})
