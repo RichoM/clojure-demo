540b4e25c156c2113bf3a7d5813e5de91d893d62
Ricardo Moran <richi.moran@gmail.com>
2021-02-27 15:04:20 -0300

    Added some more examples

--- src/clojure_demo/demo.clj
+++ src/clojure_demo/demo.clj
@@ -25,7 +25,7 @@
               "221B Baker Street"
               "Privet Drive 4"]}
 
- ;; Variables
+ ;; Vars
  (def richo {:first-name "Jorge"
               :middle-name "Ricardo"
               :last-name "Moran"
@@ -40,37 +40,59 @@
  (get richo :nick-name "Richo")
  (get-in richo [:birth-date :year])
 
+
  ;; How to modify immutable objects?
  ;; (conj, disj, assoc, dissoc, assoc-in, update, update-in, ...)
- ;; (map, filter, remove, reduce, ...)
+
  (conj [1 2 3] 4)
  (pop [1 2 3 4])
  (assoc {:a 1, :b 2}
         :c 3)
 
- (assoc-in {:first-name "Jorge"
-            :middle-name "Ricardo"
-            :last-name "Moran"
-            :birth-date {:year 1998, :month 4, :day 7}
-            :addresses ["Avenida Siempreviva 742"
-                        "221B Baker Street"
-                        "Privet Drive 4"]}
+ (assoc richo :nick-name "Richo")
+ (assoc-in richo
            [:birth-date :day-name]
            "Jueves")
 
  (update {:count 0} :count inc)
 
- (update-in {:first-name "Jorge"
-            :middle-name "Ricardo"
-            :last-name "Moran"
-            :birth-date {:year 1998, :month 4, :day 7}
-            :addresses ["Avenida Siempreviva 742"
-                        "221B Baker Street"
-                        "Privet Drive 4"]}
+ (update-in richo
             [:addresses 2] str/upper-case)
 
- ;; How to define functions? (fn, def, defn)
- ;; factorial example (*, *')
+
+ ;; Sequences (map, filter, remove, reduce, ...)
+ (map inc [1 2 3 4])
+ (filter even? [1 2 3 4 5 6 7 8 9])
+ (remove odd? [1 2 3 4 5 6 7 8 9])
+ (reduce * [1 2 3 4 5])
+
+ (set! *print-length* 1000)
+
+ (range)
+ (take 100 (filter odd? (range)))
+
+
+ ;; How to define functions? (fn, defn)
+ (defn shout [msg]
+   (str (str/upper-case msg) "!"))
+
+ (shout "Richo capo")
+
+ (defn factorial [n]
+   (if (<= n 1)
+     n
+     (* n
+        (factorial (dec n)))))
+
+ (factorial 5)
+ (map factorial (range 0 10))
+
+ (factorial 1000)
+ (/ (factorial 1000) (factorial 999))
+
+ (map (fn [n] (* n n))
+      (range 1 10))
+
 
  ;; Maps are used to represent almost everything
  ;; HTTP request/response
