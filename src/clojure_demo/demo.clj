d1e7c98c8d95083e66f692a9d2e30262717e28ce
Ricardo Moran <richi.moran@gmail.com>
2021-02-27 14:34:48 -0300

    Added some basic plan

---
+++ src/clojure_demo/demo.clj
@@ -0,0 +1,33 @@
+(ns clojure-demo.demo)
+
+(comment
+ ;; Primitive objects (numbers, ratios, strings, regex, ...) 
+
+ ;; Collections (list, vector, set, map)
+
+ ;; How to modify immutable objects?
+ ;; (conj, disj, assoc, dissoc, assoc-in, update, update-in, ...)
+ ;; (map, filter, remove, reduce, ...)
+
+ ;; How to define functions? (fn, def, defn)
+ ;; factorial example (*, *')
+
+ ;; Maps are used to represent almost everything
+ ;; HTTP request/response
+ ;; JSON
+ ;; Database rows
+ ;; HTML
+ ;; Parser
+ ;; Config
+
+ ;; What about mutable state?
+ ;; Atoms (@, swap!, reset!, ...)
+ ;; Atoms are thread-safe (data race example C# vs clj)
+
+ ;; What about polymorphism?
+ ;; Geometric shapes example
+ ;; Multimethods
+ ;; Protocols/Types/Records
+
+ ;; Future demos (macros? clojure.spec? core.async?)
+ ,)
