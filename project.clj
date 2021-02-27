aa13ed7d210c86a23cd893d825454a2b607653fe
Ricardo Moran <richi.moran@gmail.com>
2021-02-27 18:10:55 -0300

    Added dependencies for some useful libraries

--- project.clj
+++ project.clj
@@ -3,7 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [cheshire "5.9.0"] ; JSON
+                 [compojure "1.6.2"] ; routing
+                 [ring/ring-core "1.7.1"] ; web-framework
+                 [ring/ring-jetty-adapter "1.7.1"] ; jetty webserver
+                 [hiccup "1.0.5"] ; html generator
+                 ]
   :main ^:skip-aot clojure-demo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
