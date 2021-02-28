52a1865876b437285c715782002903634f798102
Ricardo Moran <richi.moran@gmail.com>
2021-02-28 17:50:43 -0300

    Improved atom example

--- src/clojure_demo/demo.clj
+++ src/clojure_demo/demo.clj
@@ -191,22 +191,40 @@
 
 
 
+ (def game-state
+   (atom {:player {:name "Ash", :level 1,
+                   :party ["Pikachu" "Pidgey" "Caterpie"]}
+          :rival {:name "Gary", :level 10,
+                  :party ["Raichu" "Pidgeot" "Butterfree"]}}))
 
- (def player (atom {:name "Ash", :level 1,
-                    :party ["Pikachu" "Pidgey" "Caterpie"]}))
+ @game-state
 
- @player
+ (swap! game-state update-in [:player :level] inc)
+ (get-in @game-state [:player :level])
 
- (swap! player update :level inc)
- (swap! player assoc :name "Red")
- (swap! player update :party conj "Charmander")
+ (swap! game-state assoc-in [:player :name] "Red")
+ (get-in @game-state [:player :name])
 
- (swap! player
-        (fn [p]
-          (-> p
-              (update :level inc)
-              (assoc :name "Gary")
-              (update :party conj "Bulbasaur"))))
+ (swap! game-state update-in [:player :party] conj "Charmander")
+ (get-in @game-state [:player :party])
+
+
+ (swap! game-state
+        #(-> %
+            (update-in [:player :level] inc)
+            (assoc-in [:player :name] "Blue")
+            (update-in [:player :party] conj "Bulbasaur")))
+
+ (macroexpand '(-> "richo"
+                   str/upper-case
+                   (str "!")
+                   println))
+
+ (fn [a] (* a a))
+ #(* % %)
+
+ (macroexpand '#(* % %))
+
 
  ;; Atoms are thread-safe (data race example C# vs clj)
 
