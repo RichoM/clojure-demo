6c99f2616c5d5c22e3d4ef654fa6a3d6055a6608
Ricardo Moran <richi.moran@gmail.com>
2021-02-28 17:51:13 -0300

    Added race condition example to show atoms' thread safety

--- src/clojure_demo/demo.clj
+++ src/clojure_demo/demo.clj
@@ -225,8 +225,30 @@
 
  (macroexpand '#(* % %))
 
+ ;; Atoms are thread-safe (data race example)
+ (def nthreads 10)
+ (def niter 500000)
+ (def counter (atom 0))
 
- ;; Atoms are thread-safe (data race example C# vs clj)
+ (defn create-thread []
+   (Thread. #(dotimes [_ niter]
+                      (swap! counter inc))))
+
+ (defn run-threads []
+   (reset! counter 0)
+   (let [threads (vec (repeatedly nthreads create-thread))]
+     (doseq [^java.lang.Thread t threads]
+       (.start t))
+     (doseq [^java.lang.Thread t threads]
+       (.join t)))
+   (let [expected (* nthreads niter)]
+     (println "ACTUAL:" @counter ", EXPECTED:" expected)
+     (println "RESULT:" (if (= @counter expected)
+                          "SUCCESS"
+                          "ERROR"))))
+
+ (run-threads)
+
 
  ;; What about polymorphism?
  ;; Geometric shapes example
