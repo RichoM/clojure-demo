925e229dd080d958fd63cf8fe5a98e6ed36c8f5b
RichoM <richi.moran@gmail.com>
2021-04-10 12:51:17 -0300

    Added some more examples for seqs and functions

--- src/clojure_demo/demo4_sequences.clj
+++ src/clojure_demo/demo4_sequences.clj
@@ -1,4 +1,5 @@
-(ns clojure-demo.demo4-sequences)
+(ns clojure-demo.demo4-sequences
+  (:require [clojure.java.io :as io]))
 
 (comment
 
@@ -8,16 +9,26 @@
  (filter even? [1 2 3 4 5 6 7 8 9])
 
  (remove odd? [1 2 3 4 5 6 7 8 9])
- 
+
  (reduce * [1 2 3 4 5])
 
 
 
-
- (set! *print-length* 1000)
+ ;; Sequences can be infinite
+ (set! *print-length* 100)
 
  (range)
- (take 100 (filter odd? (range)))
+ (take 10 (filter odd? (range)))
+
+ (repeatedly rand)
+ (iterate inc 0)
+ (file-seq (io/file "D:\\UAI\\GIRA\\UziScript"))
+
+
+
+
+
+
 
 
  ,)
--- src/clojure_demo/demo5_functions.clj
+++ src/clojure_demo/demo5_functions.clj
@@ -1,5 +1,6 @@
 (ns clojure-demo.demo5-functions
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
 
 (comment
 
@@ -10,8 +11,19 @@
  (shout "Richo capo")
 
 
+ (fn [n] (* n n))
+
+ (map (fn [n] (* n n))
+      (range 1 10))
 
 
+
+ (remove (fn [path] (str/starts-with? path "D:\\UAI\\GIRA\\UziScript\\.git"))
+         (map (fn [file] (.getPath file))
+              (file-seq (io/file "D:\\UAI\\GIRA\\UziScript"))))
+
+ (filter (fn [file] (str/ends-with? (.getName file) ".clj"))
+         (file-seq (io/file "D:\\UAI\\GIRA\\UziScript")))
 
 
 
@@ -27,7 +39,26 @@
  (factorial 1000)
  (/ (factorial 1000) (factorial 999))
 
- (map (fn [n] (* n n))
-      (range 1 10))
+ (factorial 10000)
 
+
+
+
+ (defn factorial [n]
+   (reduce *' (take-while pos-int? (iterate dec n))))
+
+ (defn factorial [n]
+   (->> n
+        (iterate dec)
+        (take-while pos-int?)
+        (reduce *')))
+
+
+(defn factorial [n]
+  (loop [i 1, f 1]
+    (if (> i n)
+      f
+      (recur (inc i) (*' i f)))))
+
+  (factorial 10000)
  ,)
