cf3a557fecd1e06494ff5e3a0c662c0a693d6198
RichoM <richi.moran@gmail.com>
2021-04-13 22:32:03 -0300

    Changed database demo to use pokemon as example

--- src/clojure_demo/demo9_database.clj
+++ src/clojure_demo/demo9_database.clj
@@ -1,18 +1,51 @@
 (ns clojure-demo.demo9-database
   (:require [clojure.pprint :refer [pprint]]
-            [conman.core
-             :refer [connect! disconnect! bind-connection]]))
+            [clojure.string :as str]
+            [conman.core :refer [connect! disconnect! bind-connection]]
+            [cheshire.core :as json]))
 
 (comment
 
  ;; Database
- (def db (connect! {:jdbc-url "jdbc:mysql://localhost:3306/clojure_demo?useSSL=false&user=guest&password=turingturing"}))
+ (def db (connect! {:jdbc-url "jdbc:mysql://localhost:3306/pokedex?useSSL=false&user=guest&password=turingturing"}))
  (bind-connection db "clojure_demo/queries.sql")
 
- (pprint (select-all-people))
- (insert-person {:nombre "Gonzalo", :apellido "Zabala" :edad 40})
- (update-person {:id 1, :nombre "Ricardo", :apellido "Moran", :edad 32})
+ (take 9 (get-all-pokemons))
+ (get-all-types)
+
+ (def pokedex (json/decode (slurp "pokedex.json")))
+
+ (def types (set (mapcat #(get % "type") pokedex)))
+
+
+ ; Populate type table
+ (doseq [type types]
+   (insert-type! {:name type}))
+
+ ; Populate pokemon table
+ (doseq [pokemon (map (fn [pokemon]
+                        (into {:id (pokemon "id"),
+                               :name (get-in pokemon ["name" "english"])}
+                              (map (fn [[key val]]
+                                     [(-> (str key)
+                                          str/lower-case
+                                          (str/replace ". " "_")
+                                          keyword)
+                                      val])
+                                   (pokemon "base"))))
+                      pokedex)]
+   (insert-pokemon! pokemon))
+
+ ; Populate pokemon_type table
+ (doseq [[id types] (map (fn [p] [(p "id") (p "type")]) pokedex)]
+   (doseq [type types]
+     (try (add-pokemon-type! {:id id, :type type})
+       (catch Throwable _))))
+
+
 
  (disconnect! db)
+
+
 
  ,)
--- src/clojure_demo/queries.sql
+++ src/clojure_demo/queries.sql
@@ -1,11 +1,24 @@
--- :name select-all-people :? :*
-select * from people;
+-- :name get-all-pokemons :? :*
+select * from pokemon;
 
--- :name insert-person :! :n
-insert into people (nombre, apellido, edad)
-values (:nombre, :apellido, :edad);
+-- :name get-all-types :? :*
+select * from type;
 
--- :name update-person :! :n
-update people
-set nombre = :nombre, apellido = :apellido, edad = :edad
-where id = :id;
+-- :name insert-type! :! :n
+insert into type (name) values (:name);
+
+-- :name insert-pokemon! :! :n
+insert into pokemon (id, name, hp, attack, defense, speed, sp_defense, sp_attack)
+values (:id, :name, :hp, :attack, :defense, :speed, :sp_defense, :sp_attack);
+
+-- :name add-pokemon-type! :! :n
+insert into pokemon_type (pokemon_id, type_id)
+values (:id, (select id from type where name = :type));
+
+-- :name get-pokemon-by-id :? :1
+select * from pokemon where id = :id;
+
+-- :name get-pokemon-types :? :*
+select t.name from type t
+inner join pokemon_type pt on pt.type_id = t.id
+where pt.pokemon_id = :id;
