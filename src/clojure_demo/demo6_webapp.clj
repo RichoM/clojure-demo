e0493c92c644cc3ba15842f774a1f5fe67963ffe
RichoM <richi.moran@gmail.com>
2021-04-14 10:15:56 -0300

    Updated the webapp with some html generation

--- src/clojure_demo/demo6_webapp.clj
+++ src/clojure_demo/demo6_webapp.clj
@@ -6,7 +6,8 @@
             [ring.util.response :refer [response]]
             [compojure.core :refer :all]
             [compojure.route :as route]
-            [cheshire.core :as json]))
+            [cheshire.core :as json]
+            [hiccup.page :as page]))
 
 (comment
 
@@ -37,11 +38,15 @@
                                 (response "OK")))
 
    ; REST
-   (GET "/api/pokemons" [] (response pokemons))
-   (GET "/api/pokemons/:id" [id] (response (find-pokemon id)))
+   (GET "/api/pokedex" [] (response pokedex))
+   (GET "/api/pokedex/:id" [id] (response (find-pokemon id)))
 
    ; HTML
-   (GET "/pokemon" [] nil)
+   (GET "/counter" [] (page/html5
+                       [:p [:div#counter 0]]
+                       [:p [:button#btn {:type "button"} "Press me!"]]
+                       [:script "btn.onclick = function () { counter.innerText++; }"]))
+   (GET "/pokemon/:id" [id] (pokemon-page (find-pokemon id)))
    (route/not-found "<h1>Page not found</h1>"))
 
  (def server (jetty/run-jetty (-> #'app
@@ -50,11 +55,49 @@
                                   wrap-json-response)
                               {:port 3000, :join? false}))
 
- (def pokemons (json/decode (slurp "pokedex.json") true))
+ (def pokedex (json/decode (slurp "pokedex.json") true))
 
  (defn find-pokemon [id]
    (first (filter (fn [p] (= id (str (:id p))))
-                  pokemons)))
+                  pokedex)))
+
+ (defn pokemon-page [{:keys [id name hp attack defense speed sp_attack sp_defense types]}]
+   (layout
+    [:div.container
+     [:div.row
+      [:div.col-auto [:h1 name]]
+      [:div.col [:h1.fst-italic (str "#" id)]]]
+     [:hr]
+     [:div.row
+      [:div.col
+       [:h3 "Types"]
+       [:ul (map (fn [t] [:li [:h4 t]]) types)]
+       [:h3 "Base stats"]
+       [:ul
+        [:li [:h4 (str "HP: " hp)]]
+        [:li [:h4 (str "Attack: " attack)]]
+        [:li [:h4 (str "Defense: " defense)]]
+        [:li [:h4 (str "Speed: " speed)]]
+        [:li [:h4 (str "Sp. Attack: " sp_attack)]]
+        [:li [:h4 (str "Sp. Defense: " sp_defense)]]]]
+      [:div.col
+       [:img.img-fluid {:src "https://i.pinimg.com/736x/bf/d8/d7/bfd8d7704cf357fdc06f003e8bfdc272.jpg"}]]]
+     [:hr]
+     [:div.row.justify-content-between
+      [:div.col-auto [:a {:href (str "/pokemon/" (inc id))} "<< Previous"]]
+      [:div.col-auto [:a {:href (str "/pokemon/" (dec id))} "Next >>"]]]]))
 
 
+ (defn layout [& content]
+   (page/html5
+    [:head
+     [:link {:href "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+             :rel "stylesheet"
+             :integrity "sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+             :crossorigin "anonymous"}]]
+    [:body
+     content
+     [:script {:src "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+               :integrity "sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
+               :crossorigin "anonymous"}]]))
  ,)
--- src/clojure_demo/demo8_html.clj
+++ src/clojure_demo/demo8_html.clj
@@ -10,29 +10,11 @@
  (html [:img {:src "imagen.jpg"}])
 
  (page/html5
-  [:p [:div#counter 0]]
-  [:p [:button#btn {:type "button"} "Press me!"]]
-  [:script "btn.onclick = function () { counter.innerText++; }"])
+  [:body
+   [:p [:div#counter 0]]
+   [:p [:button#btn {:type "button"} "Press me!"]]
+   [:script "btn.onclick = function () { counter.innerText++; }"]])
 
 
 
-
-
-
- ;; Web server + HTML
- (require '[ring.adapter.jetty :as jetty])
-
- (defn handler [request]
-   {:status 200
-    :headers {"Content-Type" "text/html"}
-    :body (page/html5
-           [:h1 [:div#counter 0]]
-           [:p [:button#btn {:type "button"} "Press me!"]]
-           [:script "btn.onclick = function () { counter.innerText++; }"])})
-
-
- (def server (jetty/run-jetty #'handler
-                              {:port 3000, :join? false}))
-
- (.stop server)
  ,)
