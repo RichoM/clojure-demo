454eccd6cea1dea86ce46231d4c594814f1c891d
RichoM <richi.moran@gmail.com>
2021-04-14 10:47:34 -0300

    Improved the pokemon page a little

--- src/clojure_demo/demo6_webapp.clj
+++ src/clojure_demo/demo6_webapp.clj
@@ -1,5 +1,6 @@
 (ns clojure-demo.demo6-webapp
   (:require [clojure.pprint :refer [pprint]]
+            [clojure.string :as str]
             [ring.adapter.jetty :as jetty]
             [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
@@ -46,6 +47,7 @@
                        [:p [:div#counter 0]]
                        [:p [:button#btn {:type "button"} "Press me!"]]
                        [:script "btn.onclick = function () { counter.innerText++; }"]))
+
    (GET "/pokemon/:id" [id] (pokemon-page (find-pokemon id)))
    (route/not-found "<h1>Page not found</h1>"))
 
@@ -65,27 +67,25 @@
    (layout
     [:div.container
      [:div.row
-      [:div.col-auto [:h1 name]]
-      [:div.col [:h1.fst-italic (str "#" id)]]]
+      [:div.col-auto [:h1.fst-italic (str "#" id)]]
+      [:div.col-auto [:h1 name]]]
      [:hr]
      [:div.row
-      [:div.col
-       [:h3 "Types"]
-       [:ul (map (fn [t] [:li [:h4 t]]) types)]
-       [:h3 "Base stats"]
-       [:ul
-        [:li [:h4 (str "HP: " hp)]]
-        [:li [:h4 (str "Attack: " attack)]]
-        [:li [:h4 (str "Defense: " defense)]]
-        [:li [:h4 (str "Speed: " speed)]]
-        [:li [:h4 (str "Sp. Attack: " sp_attack)]]
-        [:li [:h4 (str "Sp. Defense: " sp_defense)]]]]
+      [:div.col.fs-3
+       [:div.row [:div.col-5 "Types:"] [:div.col-auto.fw-light (str/join ", " types)]]
+       [:div.mt-4.fw-bold "Base stats"]
+       [:div.row [:div.col-5 "HP:"] [:div.col-auto.fw-light hp]]
+       [:div.row [:div.col-5 "Attack:"] [:div.col-auto.fw-light attack]]
+       [:div.row [:div.col-5 "Defense:"] [:div.col-auto.fw-light defense]]
+       [:div.row [:div.col-5 "Speed:"] [:div.col-auto.fw-light speed]]
+       [:div.row [:div.col-5 "Sp. Attack:"] [:div.col-auto.fw-light sp_attack]]
+       [:div.row [:div.col-5 "Sp. Defense:"] [:div.col-auto.fw-light sp_defense]]]
       [:div.col
        [:img.img-fluid {:src "https://i.pinimg.com/736x/bf/d8/d7/bfd8d7704cf357fdc06f003e8bfdc272.jpg"}]]]
      [:hr]
      [:div.row.justify-content-between
-      [:div.col-auto [:a {:href (str "/pokemon/" (inc id))} "<< Previous"]]
-      [:div.col-auto [:a {:href (str "/pokemon/" (dec id))} "Next >>"]]]]))
+      [:div.col-auto [:a {:href (str "/pokemon/" (dec id))} "<< Previous"]]
+      [:div.col-auto [:a {:href (str "/pokemon/" (inc id))} "Next >>"]]]]))
 
 
  (defn layout [& content]
