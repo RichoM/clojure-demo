35972f0908605c7c9e021ff8ada8a9536ffffdc3
RichoM <richi.moran@gmail.com>
2021-04-13 22:43:28 -0300

    Added transactions

--- src/clojure_demo/demo9_database.clj
+++ src/clojure_demo/demo9_database.clj
@@ -1,14 +1,14 @@
 (ns clojure-demo.demo9-database
   (:require [clojure.pprint :refer [pprint]]
             [clojure.string :as str]
-            [conman.core :refer [connect! disconnect! bind-connection]]
+            [conman.core :refer [connect! disconnect! bind-connection] :as conn]
             [cheshire.core :as json]))
 
 (comment
 
  ;; Database
- (def db (connect! {:jdbc-url "jdbc:mysql://localhost:3306/pokedex?useSSL=false&user=guest&password=turingturing"}))
- (bind-connection db "clojure_demo/queries.sql")
+ (def ^:dynamic *db* (connect! {:jdbc-url "jdbc:mysql://localhost:3306/pokedex?useSSL=false&user=guest&password=turingturing"}))
+ (bind-connection *db* "clojure_demo/queries.sql")
 
  (take 9 (get-all-pokemons))
  (get-all-types)
@@ -23,28 +23,30 @@
    (insert-type! {:name type}))
 
  ; Populate pokemon table
- (doseq [pokemon (map (fn [pokemon]
-                        (into {:id (pokemon "id"),
-                               :name (get-in pokemon ["name" "english"])}
-                              (map (fn [[key val]]
-                                     [(-> (str key)
-                                          str/lower-case
-                                          (str/replace ". " "_")
-                                          keyword)
-                                      val])
-                                   (pokemon "base"))))
-                      pokedex)]
-   (insert-pokemon! pokemon))
+ (conn/with-transaction [*db*]
+   (doseq [pokemon (map (fn [pokemon]
+                          (into {:id (pokemon "id"),
+                                 :name (get-in pokemon ["name" "english"])}
+                                (map (fn [[key val]]
+                                       [(-> (str key)
+                                            str/lower-case
+                                            (str/replace ". " "_")
+                                            keyword)
+                                        val])
+                                     (pokemon "base"))))
+                        pokedex)]
+     (insert-pokemon! pokemon)))
 
  ; Populate pokemon_type table
- (doseq [[id types] (map (fn [p] [(p "id") (p "type")]) pokedex)]
-   (doseq [type types]
-     (try (add-pokemon-type! {:id id, :type type})
-       (catch Throwable _))))
+ (conn/with-transaction [*db*]
+   (doseq [{id "id", types "type"} (take 5 pokedex)]
+     (doseq [type types]
+       (try (add-pokemon-type! {:id id, :type type})
+         (catch Throwable _)))))
 
 
 
- (disconnect! db)
+ (disconnect! *db*)
 
 
 
