ce903531db80edf48bb9be53504eb5c93d953d6f
RichoM <richi.moran@gmail.com>
2021-04-15 10:07:30 -0300

    Starting to make a demo for core.async

---
+++ src/clojure_demo/demo18_coreasync.clj
@@ -0,0 +1,28 @@
+(ns demo18-coreasync
+  (:require [clojure.core.async :as async :refer :all])
+  (:import [java.util Date]))
+
+(comment
+
+ ;; Multithreading
+ ;; Channels
+ ;; Asynchronous programming
+
+ (thread (dotimes [_ 5]
+                  (Thread/sleep 1000)
+                  (println (System/currentTimeMillis))))
+
+ (go (dotimes [_ 5]
+              (<! (timeout 1000))
+              (println (System/currentTimeMillis))))
+
+ (def c (chan))
+
+ (put! c "Hola")
+ (put! c "mundo")
+ (put! c "!")
+
+ (take! c println)
+
+ (<!! c)
+ ,)
